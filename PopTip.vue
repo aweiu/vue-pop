1b8e3478168bc432e181923972f9b3147e958936
余阿伟 <awei.yu@2zhuzi.com>
2016-10-27 14:53:12 +0800

    创建

---
+++ PopTip.vue
@@ -0,0 +1,136 @@
+<template>
+  <pop-tip-wrap>
+    <tip v-show="isShow" :class="{bottom:isBottom}"
+         :style="{left: left + 'px', top: top + 'px', zIndex: zIndex || 9999999}" v-el:tip>
+      <tip-body>
+        {{tip}}
+      </tip-body>
+      <tip-arrow></tip-arrow>
+    </tip>
+    <slot></slot>
+  </pop-tip-wrap>
+</template>
+<style>
+  pop-tip-wrap {
+    position: relative;
+  }
+  tip {
+    position: absolute;
+  }
+  tip-body {
+    padding: 5px 10px;
+    background-color: rgb(254, 241, 241);
+    color: #f94949;
+    font-size: 13px;
+    border-radius: 5px;
+    box-shadow: 1px 2px 1px rgb(225, 221, 222);
+    white-space: nowrap;
+  }
+  tip-arrow {
+    display: inline-block;
+    height: 8px;
+    width: 8px;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 19px;
+    margin: auto;
+    transform: rotate(45deg);
+    background-color: rgb(254, 241, 241);
+    box-shadow: 1px 2px 1px rgb(225, 221, 222);
+  }
+  tip-arrow.bottom tip-body {
+    box-shadow: 1px -2px 1px rgb(225, 221, 222);
+  }
+  tip-arrow.bottom tip-arrow {
+    top: -5px;
+    transform: rotate(225deg);
+  }
+</style>
+<script>
+  function getOffset (n1, n2) {
+    var nr1 = n1.getBoundingClientRect()
+    var nr2 = n2.getBoundingClientRect()
+    return {
+      left: nr1.left - nr2.left,
+      top: nr1.top - nr2.top
+    }
+  }
+
+  export default{
+    data () {
+      return {
+        isBottom: false,
+        left: 0,
+        top: 0,
+        index: 0,
+        record: [],
+        tips: []
+      }
+    },
+    props: ['bottom', 'zIndex'],
+    computed: {
+      tip () {
+        return this.tips[this.tips.length - 1]
+      },
+      isShow () {
+        return this.record.length > 0
+      }
+    },
+    watch: {
+      isShow (is) {
+        if (is) {
+          var wrap = this.$el
+          var tRect = this.$els.tip.getBoundingClientRect()
+          var tipHeight = tRect.height + 10
+          var tipWidth = tRect.width
+          var sRect = this.getSlotRect()
+          var offset = getOffset(this.getSlot(), wrap)
+          this.isBottom = this.bottom !== undefined || sRect.top < tipHeight
+          this.top = this.isBottom ? offset.top + sRect.height + 10 : offset.top - tipHeight
+          this.left = (offset.left + sRect.width / 2) - tipWidth / 2
+        }
+      }
+    },
+    ready () {
+      if (window.getComputedStyle(this.getSlot()).display === 'block') this.$el.style.display = 'block'
+    },
+    methods: {
+      getSlot () {
+        return this.slot || (this.slot = this.$el.lastElementChild)
+      },
+      getSlotRect () {
+        return this.getSlot().getBoundingClientRect()
+      },
+      new () {
+        var self = this
+        var index = this.index++
+        return {
+          isShow: false,
+          show (tip) {
+            if (typeof tip === 'string' && tip.length > 0) {
+              var sRect = self.getSlotRect()
+              if (sRect.height === 0 && sRect.width === 0) {
+                console.log(self)
+                return console.warn('tip can not show on an hidden popTip')
+              }
+              var aIndex = self.record.indexOf(index)
+              aIndex = (aIndex === -1 ? self.record.length : aIndex)
+              self.record.$set(aIndex, index)
+              self.tips.$set(aIndex, tip)
+              this.isShow = true
+            }
+          },
+          hide () {
+            var aIndex = self.record.indexOf(index)
+            if (aIndex !== -1) {
+              self.record.splice(aIndex, 1)
+              self.tips.splice(aIndex, 1)
+              this.isShow = false
+            }
+          }
+        }
+      }
+    }
+  }
+</script>
